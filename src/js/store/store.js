74182aa5260ec553a05b3ee20101c6b1480426b8
Phil Jerkins <Phil@TheJerkins.net>
2020-04-11 22:44:14 -0500

    Added first React middleware

--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,7 +1,11 @@
 
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "../reducers/rootReducer";
+import { forbiddenWordsMiddleware } from '../middleware/forbiddenWordsMiddleware';
 
-const store = createStore(rootReducer);
+const store = createStore(
+	rootReducer,
+	applyMiddleware(forbiddenWordsMiddleware)
+);
 
 export default store;
