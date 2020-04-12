62841fe7bbc8085f7f7a0485985b5153021652aa
Phil Jerkins <Phil@TheJerkins.net>
2020-04-11 23:14:23 -0500

    First async API call using redux-sagas

--- src/js/actions/getData.js
+++ src/js/actions/getData.js
@@ -1,10 +1,4 @@
 
 export function getData() {
-	return function(dispatch) {
-		return fetch('https://jsonplaceholder.typicode.com/posts')
-			.then(response => response.json())
-			.then(json => {
-				dispatch({ type: 'DATA_LOADED', payload: json });
-			});
-	};
+	return { type: 'DATA_REQUESTED' };
 };
--- src/js/store/store.js
+++ src/js/store/store.js
@@ -2,13 +2,20 @@
 import { createStore, applyMiddleware, compose } from "redux";
 import rootReducer from "../reducers/rootReducer";
 import { forbiddenWordsMiddleware } from '../middleware/forbiddenWordsMiddleware';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga';
+import apiSaga from '../sagas/api-saga';
+
+const initializeSagaMiddleware = createSagaMiddleware();
 
 const storeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 
 const store = createStore(
 	rootReducer,
-	storeEnhancers(applyMiddleware(forbiddenWordsMiddleware, thunk))
+	storeEnhancers(
+		applyMiddleware(forbiddenWordsMiddleware, initializeSagaMiddleware)
+	)
 );
+
+initializeSagaMiddleware.run(apiSaga);
 
 export default store;
