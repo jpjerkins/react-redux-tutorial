27493b42d860852578c71c17f5fb1a75da6bc9e2
Phil Jerkins <Phil@TheJerkins.net>
2020-04-11 22:14:45 -0500

    Renamed some files; added a bare UI on the state

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,6 @@
 
-import store from "../js/store/index";
-import { addArticle } from "../js/actions/index";
+import store from "../js/store/store";
+import { addArticle } from "../js/actions/addArticle";
 
 window.store = store;
 window.addArticle = addArticle;
--- src/js/store/store.js
+++ src/js/store/store.js
@@ -1,6 +1,6 @@
 
 import { createStore } from "redux";
-import rootReducer from "../reducers/index";
+import rootReducer from "../reducers/rootReducer";
 
 const store = createStore(rootReducer);
 
